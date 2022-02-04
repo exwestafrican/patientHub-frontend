25001a9d4a0f9db3bfe5613b4d1b0464382df5ab
tumisé <oladele.alade@gmail.com>
2022-02-04 19:29:42 +0000

    build(webpack): add html template plugin

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: {
         main: path.resolve(__dirname, "/src/main")
@@ -21,6 +23,10 @@ module.exports = {
             },
 
         ]
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    })
+    ],
 
 };
