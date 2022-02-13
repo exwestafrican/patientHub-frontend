d2c26b800768ffe12fa0898657f5bf8b34750ba6
tumisé <oladele.alade@gmail.com>
2022-02-13 18:43:04 +0000

    fix conflict for input and main

--- src/app/component/InputField/index.js
+++ src/app/component/InputField/index.js
@@ -1,6 +1,8 @@
 export default function InputField({ name, labelName, placeholderExample }) {
-    return (<div className="mb-3">
-        <label htmlFor={name} className="form-label">{labelName}</label>
-        <input type="text" className="form-control" id={name} placeholder={placeholderExample} />
-    </div>)
+    return (
+        <div className="mb-3">
+            <label htmlFor={name} className="form-label">{labelName}</label>
+            <input type="text" className="form-control" id={name} placeholder={placeholderExample} />
+        </div>
+    )
 }
--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@ import "./style.scss";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { render } from "react-dom";
 import NavBar from "./app/component/NavBar";
-<<<<<<< HEAD
-
 import Home from "./app/layout/Home";
-=======
 import PromotionCards from "./app/component/PromotionCards";
 import HospitalRegForm from "./app/component/HospitalRegForm";
->>>>>>> cd7ff7d (adding to hospital reg form)
 
 
 
