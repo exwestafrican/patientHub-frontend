f19542a84f61b5eb750f7c50d7ad71701d6c2acb
mungaiglen <mungaiglen79@gmail.com>
2022-02-14 14:06:14 +0000

    made navbar mobile responsive

--- src/app/component/NavBar/index.jsx
+++ src/app/component/NavBar/index.jsx
@@ -1,23 +1,58 @@
 import "./heading.scss";
-
+// import "./nav.js";
+import {useState} from "react";
 
 export default function NavBar() {
+    const [clx, setClx] = useState("navbar-links");
+    const [dropDown, setDropDown  ] = useState(false);
+
+
+    function handleToggleClick(){
+        console.log("click me")
+        if (dropDown){
+            console.log("if block")
+            setClx("navbar-links") 
+        }else{
+            console.log("else blocks")
+            setClx("navbar-links active")
+            
+        }
+        setDropDown(!dropDown)
+       
+    }
+  
+            
     return (
         <header className="header">
 
             <a href="#" className="logo"> <i className="fas fa-heartbeat"></i> Patient Hub </a>
 
             <nav className="navbar">
-                <a href="#home">home</a>
-                <a href="#services">services</a>
-                <a href="#about">about</a>
-                <a href="#doctors">doctors</a>
-            </nav>
-
-            <a className="cta" href="#">
+                <div className={clx}>
+                <ul>
+                <li><a href="#home">home</a></li>
+                <li><a href="#services">services</a></li>
+                <li><a href="#about">about</a></li>
+                <li><a href="#doctors">doctors</a></li>
+                
+                <li><a className="cta" href="#">
                 <button>Become a Patient</button>
+            </a></li>
+            </ul>
+            </div>
+
+
+            <a href="#" onClick={handleToggleClick} className="toggle-button">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
             </a>
 
+            </nav>
+
+           
+
+            
         </header>
     );
 }
